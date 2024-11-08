586603e107861dfbd4af761d3362733935bde040
alwierdi <alwi.erdi4@gmail.com>
2024-11-09 01:52:00 +0700

    Fix: Add default value and connecting to field on db - CategoryModel -> connect to `categories` field and add $guarded - Categories Migration -> add default value for slug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $table = 'categories';
+    protected $guarded = [];
+
     public function postRelatios()
     {
         return $this->belongsToMany(Post::class, 'category_id');
